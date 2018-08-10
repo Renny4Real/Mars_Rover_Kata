0d0780c9efc19530686ec7c3dcf19f82172d1794
Renny Fadoju <rennyvbirds@yahoo.com>
2018-08-10 15:24:13 +0100

    Edit test code for relevancy

--- spec/acceptance/mars_rover_spec.rb
+++ spec/acceptance/mars_rover_spec.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 describe 'the movement of a Mars Rover' do
-  it'can move a rover forward on square south of its position at (0,0)' do
+  it' moves a rover forward 1 space south of its initial position at (0,0), then turns left and moves 2 spaces to the east' do
     rover = Rover.new(0, 0, :S)
     rover.move(['f', 1])
+    rover.turn('l')
+    rover.move(['f', 2])
 
-    expect(rover.position).to eq(x: 0, y: 1, direction: :S)
+    expect(rover.position).to eq(x: 2, y: 1, direction: :E)
   end
 end
