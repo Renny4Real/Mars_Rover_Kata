43c7a864684bacdcba0bc394644cf268418e455d
Renny Fadoju <rennyvbirds@yahoo.com>
2018-08-10 13:49:36 +0100

    Refactor first acceptance test

--- spec/acceptance/mars_rover_spec.rb
+++ spec/acceptance/mars_rover_spec.rb
@@ -1,8 +1,8 @@
 describe 'the movement of a Mars Rover' do
-  xit'can move a rover forward on square south of its position at (0,0)' do
-    rover = Rover.new(0,0)
+  it'can move a rover forward on square south of its position at (0,0)' do
+    rover = Rover.new(0,0,:S)
     rover.move(['f', 1])
-    expect(rover.position).to eq(x: 0, y: 1 )
-    expect(rover.compass).to eq(:S)
+
+    expect(rover.position).to eq(x: 0, y: 1, direction: :S )
   end
 end
