7b79d7038e5eafc20fcef526103f376ec8e1df12
Renny Fadoju <rennyvbirds@yahoo.com>
2018-08-13 15:31:56 +0100

    Remove abbreviations used in acceptance spec.

--- spec/acceptance/mars_rover_spec.rb
+++ spec/acceptance/mars_rover_spec.rb
@@ -2,11 +2,11 @@
 
 describe 'the movement of a Mars Rover' do
   it' moves a rover forward 1 space south of its initial position at (0,0), then turns left and moves 2 spaces to the east' do
-    rover = Rover.new(0, 0, :S)
-    rover.move(['f', 1])
-    rover.turn('l')
-    rover.move(['f', 2])
+    rover = Rover.new(0, 0, :South)
+    rover.move([:forward, 1])
+    rover.turn(:left)
+    rover.move([:forward, 2])
 
-    expect(rover.position).to eq(x: 2, y: 1, direction: :E)
+    expect(rover.position).to eq(x: 2, y: 1, direction: :East)
   end
 end
